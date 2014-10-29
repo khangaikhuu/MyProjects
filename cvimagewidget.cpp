c12915c329797c95d7d44e5c9927e745ffdd3aa0
khangaikhuu <khangaikhuu@gmail.com>
2014-10-29 17:59:14 +0100

    first commit

---
+++ cvimagewidget.cpp
@@ -0,0 +1,5 @@
+#include "cvimagewidget.h"
+
+CVImageWidget::CVImageWidget()
+{
+}
---
+++ cvimagewidget.h
@@ -0,0 +1,10 @@
+#ifndef CVIMAGEWIDGET_H
+#define CVIMAGEWIDGET_H
+
+class CVImageWidget
+{
+public:
+    CVImageWidget();
+};
+
+#endif // CVIMAGEWIDGET_H
